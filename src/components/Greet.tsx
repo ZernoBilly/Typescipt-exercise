bc87582cb697660253fdcd0a82d98ce585f168a8
ZernoBilly <heikki.kurppa@gmail.com>
2021-10-20 18:39:30 +0300

    init commit

---
+++ src/components/Greet.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type GreetProps = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+
+const Greet = (props: GreetProps) => {
+  return (
+    <div>
+      {props.isLoggedIn
+        ? `Greetings ${props.name}! you have ${props.messageCount} unread messages`
+        : `Greetings Guest!`}
+    </div>
+  );
+};
+
+export default Greet;
---
+++ src/components/Person.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Person = () => {
+  return <div>Bruce Wayne</div>;
+};
+
+export default Person;
